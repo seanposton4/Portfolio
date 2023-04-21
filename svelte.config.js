108985e3fa1bf224622d2476f926fbf6cc5e6b6a
Sean Poston <sean.poston4@gmail.com>
2023-04-21 11:59:20 -0500

    fix lint errors in config

--- svelte.config.js
+++ svelte.config.js
@@ -15,17 +15,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'dist',
-			assets: 'dist',
-			fallback: null,
-			precompress: false,
-			strict: true,
-			prerender: true
+			assets: 'dist'
 		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: (dev ? '' : '/Portfolio')
 		}
 	}
 };
