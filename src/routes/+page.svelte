81ee52203bec80c96c249f7ff6103f7aefc5b452
Sean Poston <sean.poston4@gmail.com>
2023-04-21 12:48:04 -0500

    create simple coming soon page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--<h1>Welcome to SvelteKit</h1>-->
+<!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
+<div class="hero-text">
+  <h1>Coming Soon</h1>
+</div>
+
+<style>
+  .hero-text {
+    color: white;
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
