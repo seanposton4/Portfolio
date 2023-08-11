02e8736b4d2afa4487d858ee176993460399f32e
Sean Poston <sean.poston4@gmail.com>
2023-08-11 14:08:05 -0500

    remove .firebase

--- .firebase/portfolio-f4820/functions/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/entry/error.svelte.2060cbaa.js","_app/immutable/chunks/index.e65d10ad.js","_app/immutable/chunks/singletons.a677c4c2.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .firebase/portfolio-f4820/hosting/_app/immutable/chunks/2.2d30232c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.69cfe9ae.js";export{t as component};
