31ba89528bbeb2fd979b42ad7f1b12184f1577ea
Sean Poston <sean.poston4@gmail.com>
2023-08-11 13:56:08 -0500

    update to coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <!--<h1>Welcome to SvelteKit</h1>-->
 <!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
-<div class="waves-container">
+<!--<div class="waves-container">
   <div class="custom-shape-divider-bottom-1689388138">
     <svg
       data-name="Layer 1"
@@ -16,7 +16,19 @@
   </div>
 </div>
 
-<div class="text-primary">HELLO</div>
+<div class="text-primary">HELLO</div>-->
+<div class="hero-text">
+  <h1>Coming Soon</h1>
+</div>
+
+<style>
+  .hero-text {
+    color: white;
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
+
 
 <style>
 .waves-container {
