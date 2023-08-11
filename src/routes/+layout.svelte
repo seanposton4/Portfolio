e959bc550725558583cb6d5718f73a9df22bcdf9
Sean Poston <sean.poston4@gmail.com>
2023-08-11 12:54:01 -0500

    work on waves header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,3 +2,7 @@
 <!--  Hello, world!-->
 <!--</nav>-->
 <slot></slot>
+
+<!--Theme: -->
+<!--https://coolors.co/ee6c4d-f38d68-662c91-17a398-33312e-->
+<!--https://coolors.co/9ad2cb-d7ebba-feffbe-ebd494-472836-->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,50 @@
 <!--<h1>Welcome to SvelteKit</h1>-->
 <!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
-<div class="hero-text">
-  <h1>Coming Soon</h1>
+<div class="waves-container">
+  <div class="custom-shape-divider-bottom-1689388138">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z"
+        class="shape-fill"
+      ></path>
+    </svg>
+  </div>
 </div>
 
+<div class="text-primary">HELLO</div>
+
 <style>
-  .hero-text {
-    color: white;
-    font-size: 2rem;
-    text-align: center;
-  }
+.waves-container {
+  position: relative;
+  opacity: 85%;
+  height: 400px;
+}
+.custom-shape-divider-bottom-1689388138 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+}
+.custom-shape-divider-bottom-1689388138 svg {
+  position: relative;
+  display: block;
+  width: calc(109% + 1.3px);
+  height: 100px;
+}
+.custom-shape-divider-bottom-1689388138 .shape-fill {
+  fill: #333;
+}
+
+/*.hero-text {*/
+/*  color: white;*/
+/*  font-size: 2rem;*/
+/*  text-align: center;*/
+/*}*/
 </style>
