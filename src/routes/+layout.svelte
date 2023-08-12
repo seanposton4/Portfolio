10628556c8d2ba27498f94ea04a15d3d63861356
Sean <sean.poston4@gmail.com>
2023-08-12 10:09:13 -0500

    add configs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,10 @@
 <!--</nav>-->
 <nav class="p-4 bg-primary text-white flex justify-between align-middle gap-1">
   <img alt="bird-logo" src="/white-transparent-logo.png" width="100px"/>
-  <div class="grid grid-cols-4 gap-8">
+<!--  <div class="grid grid-cols-4 gap-8">
     <div>Projects</div>
     <div>About</div>
-  </div>
+  </div>-->
 </nav>
 <slot></slot>
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#151e3f',
+        'secondary': '#030027',
+        'tertiary': '#f2f3d9',
+        'accent': '#dc9e82',
+        'flair': '#c16e70'
+      }
+    },
+  },
+  plugins: [],
+}
+
